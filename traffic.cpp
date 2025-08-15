f10b3a45e1fd4a917567b72616ed1006c8cb9961
Subha-Harini-S <subha.harini@psiog.com>
2025-08-15 22:51:22 +0530

    add

---
+++ traffic.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void traffic_light(){
+    char light, car;
+    cout<<"Is traffic light present?\n";
+    cin>>light;
+    while(true){
+    
+    cout<<"Is red light is on?\n";
+    cin>>light;
+    if(tolower(light=='r')){
+        cout<<"Look left\n";
+        break;
+    }
+}
+    while(true){
+        cout<<"Does any car is approaching?";
+        cin>>car;
+    if(tolower(car=='n')){
+        cout<<"Ready to cross\n";
+        break;
+    }
+}
+}
+int main(){
+    char bridge, tunnel, cross;
+    while(true){
+        cout<<"Did you found the footbridge\n";
+        cin>>bridge;
+    if(bridge=='y'){
+        
+        cout<<"Use the foot bridge\n";
+        break;
+    }
+    cout<<"Did you found the tunnel\n";
+    cin>>tunnel;
+    if(tunnel=='y'){
+        cout<<"Use the tunnel\n";
+        break;
+    }
+    cout<<"Did you found the Crossing\n";
+    cin>>cross;
+     if(cross=='y'){
+        cout<<"Check the traffic light\n";
+        break;
+    }
+     }
+    traffic_light();
+   
+    cout<<"Cross the traffic\n";
+}
