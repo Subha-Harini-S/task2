6e7d52f0f03153878e18b333882dea5faa37732b
Subha-Harini-S <subha.harini@psiog.com>
2025-08-16 00:17:27 +0530

    added

--- grade.cpp
+++ grade.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
 
+void score(){
+    int mark;
+    cout<<"Enter the mark:\n";
+    cin>>mark;
+    if(mark<=0 && mark>=100){
+        cout<<"Invalid mark, Re-enter the mark\n";
+        score();
+    }
+    else{
+    if(mark>=80)
+            cout<<"Grade : A";
+        
+    else if(mark>=60)
+            cout<<"Grade : B";
+            
+    else if(mark>=50)
+            cout<<"Grade : C";
+            
+    else
+            cout<<"Fail";  
+    }
+}
+
 int main(){
     string revaluation;
-    cout<<"Get Score\n";
-    cout<<"Is revaluation is applied?";
-}
+    score();
+    while(true){
+          cout<<"Is revaluation is applied?\n";
+        cin>>revaluation;
+        if(tolower(revaluation=="yes"))
+        score();
+        break;
+   
+    }
+    }
